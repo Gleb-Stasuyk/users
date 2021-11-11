1fd04b51624a0e19c792993b2d1ba81f50468bff
Gleb-Stasuyk <gleb.stasuyk@gmail.com>
2021-11-11 15:33:51 +0300

    add password hashing in userSerializer before creating that fixed bug in token getting

--- api/v1/serializers.py
+++ api/v1/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from rest_framework import  serializers
 
 
@@ -17,6 +18,17 @@ class UserSerializer(serializers.ModelSerializer): # pylint: disable=too-few-pub
         )
     is_active = serializers.BooleanField(required=True)
 
+
+    def validate_password(self, value: str) -> str:
+        """
+        Hash value passed by user.
+
+        :param value: password of a user
+        :return: a hashed version of the password
+        """
+        return make_password(value)
+
+
     class Meta:
         model = User
         fields = [
@@ -24,3 +36,4 @@ class UserSerializer(serializers.ModelSerializer): # pylint: disable=too-few-pub
             'is_active', 'last_login', 'is_superuser', 'password'
             ]
         read_only_fields = ['id', 'last_login', 'is_superuser']
+
